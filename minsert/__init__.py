d779d621a91d2f73bd1f79fb5ce04a2b228082eb
aahnik <daw@aahnik.dev>
2021-06-07 19:07:19 +0530

    add minsert

---
+++ minsert/__init__.py
@@ -0,0 +1,16 @@
+"""Package minsert.
+
+Insert dynamic content in markdown, without using a separate template file.
+
+MIT License
+
+Copyright (c) 2021 Aahnik Daw
+
+https://github.com/aahnik/minsert
+"""
+
+from importlib.metadata import version
+
+from minsert.minsert import MarkdownFile
+
+__version__ = version(__package__)
